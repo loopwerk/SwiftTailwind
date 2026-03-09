c6d718ba6245703122d00c082045fa07ca325561
Kevin Renskers <kevin@loopwerk.io>
2026-03-09 15:53:21 +0100

    chore: Update the minimum Swift version to 5.10

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.10
 import PackageDescription
 
 let package = Package(
   name: "SwiftTailwind",
-  platforms: [.macOS(.v12)],
+  platforms: [.macOS(.v14)],
   products: [
     .library(name: "SwiftTailwind", targets: ["SwiftTailwind"]),
   ],
